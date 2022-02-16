fbc17e78f0d9a478259c6cbed67ba85715f80bbb
CarolinaCasas <casasc9@uabc.edu.mx>
2022-02-15 21:52:53 -0600

    checkInput

---
+++ src/components/taskReducer.js
@@ -0,0 +1,11 @@
+
+export const taskReducer=(state=[], action)=>{
+
+    switch (action?.type) {
+         case 'add':
+            return [...state,action.payload];
+
+        default:
+            return state;
+    }
+}

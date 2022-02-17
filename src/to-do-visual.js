c34bb23e29d62244a1aa193118cc04e1c6aa839a
CarolinaCasas <casasc9@uabc.edu.mx>
2022-02-16 23:45:17 -0600

    design-background

--- src/to-do-visual.js
+++ src/to-do-visual.js
@@ -1,5 +1,6 @@
 import './to-do-list.css';
 import logo from './img/logo.png';
+import trash from  './img/trash.png'
 
 export function ToDoVisual(props) {
 
@@ -10,8 +11,8 @@ export function ToDoVisual(props) {
         <header className='App-header'>
           <img src={logo} className='App-logo' alt='logo' />
         </header>
-        <main>
-          <p>{tasks.length}</p>
+        <main className='principal-content'>
+          <p className='message'>Hello! You have {tasks.length} pending tasks</p>
           <form
             className='new-task'
             onSubmit={handleAddTask}>
@@ -25,12 +26,19 @@ export function ToDoVisual(props) {
           <ul>
             {
               tasks.map(task =>
-              (<li
+              (<li 
                 key={task.id}
                 className='list-item'>
-                <input id={task.id} type='checkbox' value={task.taskDescription} onClick={(e)=>console.log(e.target.checked)}/>
+                <input
+                 id={task.id}
+                 type='checkbox' 
+                 value={task.taskDescription} 
+                 onClick={(e)=>console.log(e.target.checked)}/>
                 <label htmlFor={task.id}> {task.taskDescription} </label>
-                <button onClick={()=>deleteTask(task.id)}>delete</button>
+                <img 
+                src={trash} alt='delete' 
+                className='trash' 
+                onClick={()=>deleteTask(task.id)}/>
               </li>)
               )
             }
